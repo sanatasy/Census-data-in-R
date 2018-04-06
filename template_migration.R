898fc0e12f0037455c09b1bc2263a68c9892c3f7
Sanata Sy <36007933+sanatasy@users.noreply.github.com>
2018-04-06 15:12:54 -0400

    Update template_migration.R

--- template_migration.R
+++ template_migration.R
@@ -1,9 +1,13 @@
-load("C:\\Users\\Sanata\\Dropbox\\08 Migration Paper\\Datasets\\01 Censuses\\cameroon_census05_edited.RData")
 
 ##R file for migration paper 
 ##Started 09/2015 
 ##Uses IPUMS census data 
 ##This is the template file, which uses Zambia as example 
+##The main tasks this file executes are: 
+##1) determine birthplace and current location mismatches 
+##2) identify the type of move that occurred (across regions (level 1) or across districts (level 2)) 
+##3) calculate the proportion of regional and district movers by administrative unit 
+##4) export the resulting migration dataset to RData 
 
 #### Packages ##### 
 require(foreign)
@@ -16,6 +20,7 @@ seed <- "C:\\Users\\Sanata\\Dropbox\\08 Migration Paper\\IPUMS Migration\\"
 location <- paste0(seed, "Zambia\\")
 setwd(location)
 
+#set country and census years 
 country <- "zambia"
 cname <- "Zambia"
 years <- c("90","00", "10")
@@ -24,7 +29,7 @@ years <- c("90","00", "10")
 
 
 ####### Read file ############
-year <- years[3]
+year <- years[1]  # select a census year 
 zam <- read.dta13( paste0(location, country, "_census", year, ".dta"), convert.factors = F, 
                    convert.underscore = T)
 
@@ -180,180 +185,8 @@ save(bpl.district.mobility, file=paste0(saveloc, "05 Subreg Mobility\\", country
 save(bpl.regional.mobility, file=paste0(saveloc, "04 Reg Mobility\\", country, year, ".bpl.reg.mobility", ".RData"))
 
 
-#####################################
 
 
-
-
-################ OLD CODE STARTS HERE #########################
-
-##############################################################
-
-
-
-
-#### Functions ######## 
-
-my.ci <- function(mean, sd, n){
-  m <- mean
-  s <- sd
-  n <- n
-  error <- qnorm(0.975)*s/sqrt(n)
-  lower <- m - error
-  upper <- m + error 
-  output <- t(c(lower, upper))
-  output <- as.data.frame(output)
-  colnames(output) <- c("lower", "upper")
-  output
-}
-
-
-
-###########################
-movers.tab.dist <- table(zam$moved, zam$geo2b.zm)[2, ]
-movers.dist <- as.vector(movers.tab.dist)
-names(movers.dist) <- names(movers.tab.dist)
-
-movers.tab.const <- table(zam$moved, zam$constzm)[2, ]
-movers.const <- as.vector(movers.tab.const)
-names(movers.const) <- names(movers.tab.const)
-
-movers.tab.bpl <- table(zam$moved, zam$bplzm)[2, ]
-movers.bpl <- as.vector(movers.tab.bpl)
-names(movers.bpl ) <- names(movers.tab.bpl)
-
-
-#Export 
-prop.moved.dist00 <- movers.dist / districts
-prop.moved.const00 <- movers.const / const 
-prop.moved.bpl00 <- movers.bpl / bpls
-
-write.csv(prop.moved.const00, file="movers00const.csv")
-write.csv(prop.moved.dist00, file="movers00dist.csv")
-write.csv(prop.moved.bpl00, file = "movers00bpl.csv" )
-
-prop.moved.dist10 <- movers.dist / districts
-prop.moved.const10 <- movers.const / const 
-prop.moved.bpl10 <- movers.bpl / bpls
-
-write.csv(prop.moved.const10, file="movers10const.csv")
-write.csv(prop.moved.dist10, file="movers10dist.csv")
-write.csv(prop.moved.bpl10, file = "movers10bpl.csv" )
-
-
-
-
-
-##calculate birthplace of person i score minus current place of person i score 
-##For each i, get "birthplace name"'s score 
-
-mig <-subset(fullpop, fullpop$moved==1) 
-
-#make unique district matrix with district proportions 
-stats.f <- cbind(unique(fullpop$geo2b.zm), unique(fullpop$prop.radio), 
-                 unique(fullpop$prop.elctr), unique(fullpop$prop.urban), 
-                 unique(fullpop$toilet.score), unique(fullpop$watsup.score))
-colnames(stats.f) <- c("distid", "prop.radio", "prop.elctr", "prop.urban", "toilet.score", "watsup.score")
-stats.f <- as.data.frame(stats.f)
-
-
-
-#make container vectors for electricity and urban 
-electr.p <- rep(NA, length(unique(mig$geo2b.zm)))
-urban.p <- rep(NA, length(unique(mig$geo2b.zm)))
-                                
-for(i in 1:length(unique(mig$geo2b.zm))){
-  
-  sub <- subset(mig, mig$geo2b.zm==unique(fullpop$geo2b.zm)[i])
-  electr.p [i] <- sub$prop.elctr[1]
-  urban.p[i] <- sub$prop.urban[1] 
-  
-}
-
-
-stats.m <- cbind(unique(mig$geo2b.zm), unique(mig$toilet.score), unique(mig$prop.tv),
-                 unique(mig$watsup.score), electr.p, urban.p)
-
-
-colnames(stats.m) <- c("distid",  "toilet.score", "prop.tv", "watsup.score", "prop.elctr", "prop.urban")
-stats.m <- as.data.frame(stats.m)
-
-
-
-#container vectors for individuals' birthplace scores 
-bpl.prop.tv <- rep(NA, nrow(mig))
-bpl.toilet.score <- rep(NA, nrow(mig))
-bpl.watsup.score <- rep(NA, nrow(mig))
-bpl.prop.elctr <- rep(NA, nrow(mig))
-bpl.prop.urban <- rep(NA, nrow(mig))
-  
-#for loop to get individual's birthplace scores  
-
-for(i in 1:nrow(mig)){
-  
-  #find the individual's birthplace that matches the list of unique birthplaces 
-  bplid <- match(mig$bplzm[i], stats.m$distid)
-  bpl.prop.tv[i] <- stats.m$prop.tv[bplid]
-  bpl.toilet.score[i] <- stats.m$toilet.score[bplid] 
-  bpl.watsup.score[i] <- stats.m$watsup.score[bplid]
-  bpl.prop.elctr[i] <- stats.m$prop.elctr[bplid] 
-  bpl.prop.urban[i] <- stats.m$prop.urban[bplid] 
-  
-  
-}
-
-#add to data frame 
-mig$bpl.prop.tv <- bpl.prop.tv
-mig$bpl.toilet.score <- bpl.toilet.score 
-mig$bpl.watsup.score <- bpl.watsup.score
-mig$bpl.prop.elctr <- bpl.prop.elctr
-mig$bpl.prop.urban <- bpl.prop.urban
-
-#get differences 
-mig$tv.diff <- mig$prop.tv - mig$bpl.prop.tv 
-mig$toilet.diff <- mig$toilet.score - mig$bpl.toilet.score
-mig$water.diff <- mig$watsup.score -  mig$bpl.watsup.score
-mig$elctr.diff <- mig$prop.elctr - mig$bpl.prop.elctr
-mig$urban.diff <- mig$prop.urban - mig$bpl.prop.urban
-
-
-
-#### Graph point estimates for birthplace and current place ### 
-
-#Among those who moved up 
-means <- colMeans(mig[mig$urban.diff>=0, c("bpl.prop.elctr", "prop.elctr", 
-                          "bpl.prop.tv", "prop.tv",
-                          "bpl.toilet.score", "toilet.score", 
-                          "bpl.prop.urban", "prop.urban",
-                          "bpl.watsup.score", "watsup.score")],na.rm = T)
-
-
-
-#Plot  Means 
-s <- sd(mig$bpl.prop.tv, na.rm=T)
-n <- length(mig$bpl.prop.tv)
-ci.mig <- my.ci(means[4], s, n)
-ci.birth <- my.ci(means[3], s, n)
-
-plot(c(1, 2), means[3:4], main = "Comparing Birth and Migration Districts", 
-     ylab = "Proportions", xlab = "Districts", 
-     ylim = c(0, 1), xlim = c(0, 3),
-     pch = 16, type="b", xaxt = "n")
-lines(c(1,1), c(ci.birth$lower, ci.birth$upper ))
-lines(c(2,2), c(ci.mig$lower, ci.mig$upper ))
-#urban
-lines(c(1,2), means[7:8], pch=16, type="b", col="blue")
-#electricity 
-lines(c(1,2), means[1:2], pch=16, type="b", col="green")
-#toilet 
-lines(c(1,2), means[5:6], pch=16, type="b", col="red")
-#water
-lines(c(1,2), means[9:10], pch=16, type="b", col="purple")
-axis(side = 1, at = c(1, 2), tick = FALSE,
-     labels = c("Birth District", "Migration District"))
-
-
- 
 
 
 
